d4dbc6f932e5a9d40bc3072274bc08ea2db587d9
Nikita Burakov <i@toshiyoung.com>
2020-02-26 01:05:54 +0300

    Коннектор Базовые методы Метод Settings

---
+++ src/methods/Settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * *
+ *  * @author Nikita Burakov <i@toshiyoung.com>
+ *  * Copyright (c) 2020.
+ *
+ */
+
+namespace Remake\UDSConnector\Methods;
+
+class Settings extends BaseMethod {
+
+    public $methodPath = 'settings';
+}
+
