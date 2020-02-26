f991f53988a539ec129950e0ce028e78c8dad66c
Toshi Young <i@toshiyoung.com>
2020-02-26 13:06:13 +0300

    Доработка Settings и тестирование

---
+++ src/methods/Operations.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Nikita Burakov <i@toshiyoung.com>
+ * Copyright (c) 2020
+ */
+
+namespace Remake\UDSConnector\Methods;
+
+class Operations extends BaseMethod {
+
+    public $methodPath = 'operations';
+}
+
