aceb268d1f595b26841446b46d005aa0cf905cef
Nikita Burakov <i@toshiyoung.com>
2020-02-27 00:42:36 +0300

    In Progress use Builder Template

---
+++ src/Methods/Operations.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * *
+ *  * @author Nikita Burakov <i@toshiyoung.com>
+ *  * Copyright (c) 2020.
+ *
+ */
+
+namespace Remake\UDSConnector\Methods;
+
+use Remake\UDSConnector\Client;
+
+Class Operations extends Client {
+    protected $methodPath = 'operations';
+}
---
+++ src/Methods/Settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * *
+ *  * @author Nikita Burakov <i@toshiyoung.com>
+ *  * Copyright (c) 2020.
+ *
+ */
+namespace Remake\UDSConnector\Methods;
+
+use Remake\UDSConnector\Client;
+
+Class Settings extends Client
+{
+    protected $methodPath = 'settings';
+
+}
